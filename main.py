832de7861004b28f430b6a53856d35e3f93ee859
Rana Abdulrehman <rabdulrehman2674@gmail.com>
2026-03-29 20:59:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ model = pickle.load(open('model.pkl','rb'))
 
 st.title("Email Spam Classifier")
 
-input_sms = st.text_area("Enter the Mailll:")
+input_sms = st.text_area("Enter the Mail:")
 st.write(model)
 
 if st.button("Predict"):
